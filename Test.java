2242e8b1020d3c1ff4221822e2a58abf3d2fec9f
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-01-18 19:16:25 +0530

    add greeting method

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
-public class Test{
+public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Program started..");
+        greeting();
+        System.out.println("Program ended..");
+    }
+
+    public static void greeting() {
+        System.out.println("Hello, How are you?");
     }
 }
