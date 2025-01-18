8918e1a1bab5a9ea968508cc241d18c8fc0637ab
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-01-18 20:39:15 +0530

    add print method

--- Test.java
+++ Test.java
@@ -2,10 +2,15 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Program started..");
         greeting();
+        print();
         System.out.println("Program ended..");
     }
 
     public static void greeting() {
         System.out.println("Hello, How are you?");
+    }
+
+    public static void print() {
+        System.out.println("Printing..");
     }
 }
